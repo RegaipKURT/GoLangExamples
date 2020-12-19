756337838b80f8e11728685e9b83d2634389d9e3
Regaip KURT <regkrt@gmail.com>
2020-12-19 21:17:07 +0300

    Merkez Bankası Döviz Kurlarını Gösteren Uygulama

--- mbdoviz.go
+++ mbdoviz.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Currency struct {
@@ -32,25 +33,30 @@ func main() {
 	defer resp.Body.Close()
 	data, _ := ioutil.ReadAll(resp.Body)
 	data_n := string(data)
-	
+
 	var kurlar struct {
-		Tarih_Date 	[]Currency `xml:"Currency"`
+		Tarih_Date []Currency `xml:"Currency"`
 	}
-	
+
 	if err := xml.Unmarshal([]byte(data_n), &kurlar); err != nil {
 		log.Fatal(err)
 	}
+
 	if len(os.Args[1:]) == 0 {
 		for _, c := range kurlar.Tarih_Date[:len(kurlar.Tarih_Date)-1] {
-			fmt.Println(c.Isim, "("+c.CurrencyName+")", "-  Alış:",
-				c.ForexBuying, "-Satış:", c.ForexSelling)
+			fmt.Println(c.Isim, "("+c.CurrencyName+")", " Alış:",
+				c.ForexBuying, "- Satış:", c.ForexSelling)
+		}
+		for _, c := range kurlar.Tarih_Date[len(kurlar.Tarih_Date)-1 : len(kurlar.Tarih_Date)] {
+			fmt.Println("Special Drawing Rights (SDR)", " Alış:",
+				c.ForexBuying, "- Satış:", c.ForexSelling)
 		}
 	} else {
 		for _, isim := range os.Args[1:] {
 			for _, c := range kurlar.Tarih_Date {
-				if isim == c.Isim || isim == c.CurrencyName {
-					fmt.Println(c.Isim, "("+c.CurrencyName+")", "-  Alış:",
-						c.ForexBuying, "-Satış:", c.ForexSelling)
+				if strings.ToUpper(isim[:2]) == c.Isim[:2] || strings.ToUpper(isim[:2]) == c.CurrencyName[:2] {
+					fmt.Println(strings.Replace(c.Isim, "  ", "", -1), "("+strings.Replace(c.CurrencyName, "  ", "", -1)+")", " Alış:",
+						c.ForexBuying, "- Satış:", c.ForexSelling)
 				}
 			}
 		}
