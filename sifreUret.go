7cc35c0760c02b25d677336f9c182d5baf577099
Regaip KURT <regkrt@gmail.com>
2022-01-06 16:10:34 +0300

    Update sifreUret.go

--- sifreUret.go
+++ sifreUret.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"time"
@@ -11,20 +12,53 @@ import (
 var source = rand.NewSource(time.Now().UnixNano())
 
 //şifremiz bu karakterler arasından seçilecek sembollerle oluşturulacak!
-const _charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*!+.()&%#[]{}=;:"
+const _charsetLow = "abcdefghijklmnopqrstuvwxyz"
+const _charsetMidLow = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const _charsetMidHigh = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const _charsetHigh = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*!+.()&%#[]{}=;:"
+
+var L, C int
 
 func main() {
+	//şifre uzunluğunu argüman alma
+
+	L := flag.Int("length", 4, "Length of password")
+	//şifre kompleksliği argümanı
+	C := flag.Int("complex", 4, "Complexity of password")
+	flag.Parse()
+	fmt.Println(*L, *C)
 	//sifre üretme fonksiyonunu çağırıp sifre değerine atayalım ve yazdıralım.
-	sifre := GeneratePassword(32) // 32 karakterli bir string dönecek
+	sifre := GeneratePassword(*L, *C) // karakterli bir string dönecek
 	fmt.Println("Üretilen Şifre:", sifre)
 }
 
 // GeneratePassword : Uzunluk(length) değeri alıp o uzunlukta rasgele şifre üretir.
 // Dönüş tipi string'dir
-func GeneratePassword(length int) string {
+func GeneratePassword(length, complexity int) string {
 	x := make([]byte, length) //x byte dizisi içine ekleyeceğiz seçtiğimiz karakterleri
-	for i := range x {
-		x[i] = _charset[source.Int63()%int64(len(_charset))]
+	if complexity <= 1 {
+		for i := range x {
+			x[i] = _charsetLow[source.Int63()%int64(len(_charsetLow))]
+		}
+		return string(x)
+	}
+	if complexity == 2 {
+		for i := range x {
+			x[i] = _charsetMidLow[source.Int63()%int64(len(_charsetMidLow))]
+		}
+		return string(x)
+	}
+	if complexity == 3 {
+		for i := range x {
+			x[i] = _charsetMidHigh[source.Int63()%int64(len(_charsetMidHigh))]
+		}
+		return string(x)
+	}
+	if complexity >= 4 {
+		for i := range x {
+			x[i] = _charsetHigh[source.Int63()%int64(len(_charsetHigh))]
+		}
+		return string(x)
 	}
 	return string(x) //byte dizisini stringe çevirip döndürüyoruz!
 }
